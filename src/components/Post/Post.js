8e058f2cbbd1ae7d307fd8e8a52c121ed061be60
callumskilling <88871549+callumskilling@users.noreply.github.com>
2023-02-12 10:51:40 +1100

    added post specific comment lost

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Dispatch } from '@reduxjs/toolkit';
 import { timeFromNow } from '../../utils/TimeFromNow';
 import CommentsList from '../../features/commentsList/CommentsList'
 import './Post.css'
+import { useDispatch } from 'react-redux';
+
+
 
 export default function Post({ post }) {
     const unixTimestamp = post.created;
+    const postUrl = post.url
+    const dispatch = useDispatch();
     return (
-        <div key={post.id} className='post-container'>
+        <div key={post.id} className='post-container' onClick={() => dispatch({type: "commentsList/updateCurrentPost", payload: `${postUrl}`})}>
             <h3 className='post-title'>{post.title}</h3>
             <img src={post.thumbnail} alt='' className='post-thumbnail' />
             <div className='post-info'>
